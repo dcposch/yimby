a342ec60edfc23d847b386b08f3134cc3be10aa6
DC <dcposch@dcpos.ch>
2017-09-16 20:45:38 -0700

    yimby mapper improvements
    you can export csv, tsv, or email To: format now
    
    multiple contacts in the same building are shown separately

--- src/contacts/controls.js
+++ src/contacts/controls.js
@@ -2,108 +2,220 @@ import React from 'react'
 
 // import districts from './districts.js'
 
-export default function Controls (props) {
-  const {
-    isLoggedIn,
-    onLogOut,
-    contacts,
-    filteredContacts,
-    filterExport,
-    radius,
-    radiusAddress,
-    district,
-    onChangeDistrict,
-    onChangeRadius,
-    onChangeRadiusAddress
-  } = props
+export default class Controls extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      exportFormat: 'csv' // other options: 'tsv', 'email'
+    }
+  }
 
-  const elemLogin = isLoggedIn
-    ? <button onClick={onLogOut}>log out</button>
-    : <a href='https://login.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9szVa2RxsqBYHAPh..zyh4EVbe.lQAkzoWqg8MoVL4ttsXhCu0YlLbISt_OAPP16XvT3v0e0tm.JMATvx
-&redirect_uri=https://dcpos.ch/yimby/contacts/'>log in</a>
+  render () {
+    const { exportFormat } = this.state
+    const {
+      isLoggedIn,
+      onLogOut,
+      contacts,
+      filteredContacts,
+      radius,
+      radiusAddress,
+      district,
+      onChangeDistrict,
+      onChangeRadius,
+      onChangeRadiusAddress
+    } = this.props
 
-  const elemsDistrictFilter = [1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11]
-    .map((i, j) => {
+    const elemLogin = isLoggedIn ? (
+      <button onClick={onLogOut}>log out</button>
+    ) : (
+      <a href='https://login.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9szVa2RxsqBYHAPh..zyh4EVbe.lQAkzoWqg8MoVL4ttsXhCu0YlLbISt_OAPP16XvT3v0e0tm.JMATvx
+&redirect_uri=https://dcpos.ch/yimby/contacts/'>
+        log in
+      </a>
+    )
+
+    const elemsDistrictFilter = [
+      1,
+      0,
+      2,
+      0,
+      3,
+      0,
+      4,
+      0,
+      5,
+      0,
+      6,
+      0,
+      7,
+      0,
+      8,
+      0,
+      9,
+      0,
+      10,
+      0,
+      11
+    ].map((i, j) => {
       if (i === 0) return ' '
       var c = district === i ? 'selected' : ''
-      return <span key={j} className={c} onClick={() => onChangeDistrict(i)}>{'D' + i}</span>
+      return (
+        <span key={j} className={c} onClick={() => onChangeDistrict(i)}>
+          {'D' + i}
+        </span>
+      )
     })
 
-  let elemRadiusFilter = null
-  if (radiusAddress) {
-    const r = radius
-    elemRadiusFilter = (
+    let elemRadiusFilter = null
+    if (radiusAddress) {
+      const r = radius
+      elemRadiusFilter = (
+        <div>
+          <p>
+            {radiusAddress.success ? (
+              <span>{radiusAddress.name}</span>
+            ) : (
+              "couldn't geocode address"
+            )}
+          </p>
+          <p className='legend'>
+            <span
+              className={r === 100 ? 'selected' : ''}
+              onClick={() => onChangeRadius(100)}
+            >
+              100m
+            </span>{' '}
+            <span
+              className={r === 200 ? 'selected' : ''}
+              onClick={() => onChangeRadius(200)}
+            >
+              200m
+            </span>{' '}
+            <span
+              className={r === 500 ? 'selected' : ''}
+              onClick={() => onChangeRadius(500)}
+            >
+              500m
+            </span>{' '}
+            <span
+              className={r === 1000 ? 'selected' : ''}
+              onClick={() => onChangeRadius(1000)}
+            >
+              1km
+            </span>
+          </p>
+          <p>click above to filter.</p>
+        </div>
+      )
+    }
+
+    // export contact emails
+    let elemExportFormat = (
       <div>
-        <p>
-          { radiusAddress.success
-              ? <span>{radiusAddress.name}</span>
-              : 'couldn\'t geocode address' }
-        </p>
         <p className='legend'>
-          <span className={r === 100 ? 'selected' : ''} onClick={() => onChangeRadius(100)}>100m</span>{' '}
-          <span className={r === 200 ? 'selected' : ''} onClick={() => onChangeRadius(200)}>200m</span>{' '}
-          <span className={r === 500 ? 'selected' : ''} onClick={() => onChangeRadius(500)}>500m</span>{' '}
-          <span className={r === 1000 ? 'selected' : ''} onClick={() => onChangeRadius(1000)}>1km</span>
-        </p>
-        <p>
-          click above to filter.
+          <span
+            className={exportFormat === 'csv' ? 'selected' : ''}
+            onClick={() => this.setState({ exportFormat: 'csv' })}
+          >
+            csv
+          </span>{' '}
+          <span
+            className={exportFormat === 'tsv' ? 'selected' : ''}
+            onClick={() => this.setState({ exportFormat: 'tsv' })}
+          >
+            tsv
+          </span>{' '}
+          <span
+            className={exportFormat === 'email' ? 'selected' : ''}
+            onClick={() => this.setState({ exportFormat: 'email' })}
+          >
+            email
+          </span>
         </p>
       </div>
     )
+    let filterExport = formatContacts(filteredContacts, exportFormat)
+    /* if (filteredContacts.length === contacts.length) {
+      elemExportFormat = (
+        <span>filter down to fewer contacts to see export</span>
+      )
+      filterExport = null
+    } else {
+      filterExport =
+    } */
+
+    return (
+      <div className='summary'>
+        <h1>yimby contacts</h1>
+        <p>
+          here's where our contacts live. click on any of the dots for details.
+          missing addresses are to the left, by ocean beach.
+        </p>
+
+        <h2>salesforce api</h2>
+        <p>
+          certified yimby party members only. if you have access to the yimby
+          salesforce, you can authorize this page to map contacts.
+        </p>
+        <p>{elemLogin}</p>
+        <p>
+          loaded <strong>{contacts.length}</strong> contacts with valid
+          addresses
+        </p>
+        <p>
+          {filteredContacts.length === contacts.length ? (
+            'showing all contacts'
+          ) : (
+            'filtered down to ' + filteredContacts.length + ' contacts'
+          )}
+        </p>
+
+        <h2>district</h2>
+        <p className='legend'>{elemsDistrictFilter}</p>
+        <p>click on a district above to filter. click again to un-filter.</p>
+
+        <h2>radius</h2>
+        <p>
+          enter an address:{' '}
+          <input
+            className='radius-address'
+            onChange={e => onChangeRadiusAddress(e.target.value)}
+          />
+        </p>
+        {elemRadiusFilter}
+
+        <h2>export</h2>
+        {elemExportFormat}
+        <pre>{filterExport}</pre>
+
+        <footer>
+          code on <a href='//github.com/dcposch/yimby'>github</a>. twitter:{' '}
+          <a href='//twitter.com/dcposch'>@dcposch</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+function formatContacts (contacts, format) {
+  if (format === 'email') {
+    return contacts
+      .filter(c => !!c.email)
+      .map(c => c.name + '<' + c.email + '>')
+      .join(', ')
   }
 
-  return (
-    <div className='summary'>
-      <h1>yimby contacts</h1>
-      <p>
-        here's where our contacts live. click on any of the dots for details.
-        missing addresses are to the left, by ocean beach.
-      </p>
-
-      <h2>salesforce api</h2>
-      <p>
-        certified yimby party members only. if you have access to the
-        yimby salesforce, you can authorize this page to map contacts.
-      </p>
-      <p>
-        {elemLogin}
-      </p>
-      <p>
-        loaded <strong>{contacts.length}</strong> contacts with valid addresses
-      </p>
-      <p>
-        {filteredContacts.length === contacts.length
-          ? 'showing all contacts'
-          : ('filtered down to ' + filteredContacts.length + ' contacts')}
-      </p>
-
-      <h2>district</h2>
-      <p className='legend'>
-        {elemsDistrictFilter}
-      </p>
-      <p>
-        click on a district above to filter. click again to un-filter.
-      </p>
-
-      <h2>radius</h2>
-      <p>
-        enter an address: <input
-          className='radius-address'
-          onChange={(e) => onChangeRadiusAddress(e.target.value)}
-        />
-      </p>
-      { elemRadiusFilter }
-
-      <h2>export</h2>
-      <pre>
-        {filterExport}
-      </pre>
-
-      <footer>
-        yimby digital liberation army.
-        code on <a href='//github.com/dcposch/yimby'>github</a>.
-        twitter: <a href='//twitter.com/dcposch'>@dcposch</a>
-      </footer>
-    </div>
-  )
+  const delimiter = format === 'csv' ? ',' : '\t'
+  return contacts
+    .map(c =>
+      [c.name, c.email, c.phone]
+        .map(val =>
+          val
+            .replace('\n', '')
+            .replace(delimiter, '')
+            .trim()
+        )
+        .join(delimiter)
+    )
+    .join('\n')
 }
--- src/contacts/index.js
+++ src/contacts/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ScatterplotLayer} from 'deck.gl'
+import { ScatterplotLayer } from 'deck.gl'
 
 import config from '../config'
 import fetch from '../fetch'
@@ -10,6 +10,113 @@ import Controls from './controls'
 
 const API_ROOT = 'https://na35.salesforce.com/services/data/v40.0'
 
+const API_SAMPLE_RESPONSE = {
+  totalSize: 3,
+  done: true,
+  records: [
+    {
+      attributes: {
+        type: 'Contact',
+        url: '/services/data/v40.0/sobjects/Contact/0034100000Qqhs7AAB'
+      },
+      Name: 'Ravi Sankar',
+      Phone: '1234567890',
+      Email: 'test@example.com',
+      MailingAddress: {
+        city: 'San Francisco',
+        country: 'US',
+        countryCode: null,
+        geocodeAccuracy: null,
+        latitude: 37.7595554,
+        longitude: -122.4291003,
+        postalCode: '94114',
+        state: 'CA',
+        stateCode: null,
+        street: '3835 19th St'
+      },
+      Administrative_Area__c: '8',
+      State_Upper_District__c: '11',
+      State_Lower_District__c: '17',
+      npo02__TotalOppAmount__c: 12345
+    },
+    {
+      attributes: {
+        type: 'Contact',
+        url: '/services/data/v40.0/sobjects/Contact/0034100000NrSgvAAF'
+      },
+      Name: 'DC Posch',
+      Phone: '8012345678',
+      Email: 'dc1@example.com',
+      MailingAddress: {
+        city: 'San Francisco',
+        country: 'US',
+        countryCode: null,
+        geocodeAccuracy: null,
+        latitude: 37.7779536,
+        longitude: -122.4067138,
+        postalCode: '94103',
+        state: 'CA',
+        stateCode: null,
+        street: '1040 Folsom St'
+      },
+      Administrative_Area__c: '6',
+      State_Upper_District__c: '11',
+      State_Lower_District__c: '17',
+      npo02__TotalOppAmount__c: 12345
+    },
+    {
+      attributes: {
+        type: 'Contact',
+        url: '/services/data/v40.0/sobjects/Contact/0034100000NrSgvAAF'
+      },
+      Name: 'Dan Posch',
+      Phone: '8012345678',
+      Email: 'dc2@example.com',
+      MailingAddress: {
+        city: 'San Francisco',
+        country: 'US',
+        countryCode: null,
+        geocodeAccuracy: null,
+        latitude: 37.7779536,
+        longitude: -122.4067138,
+        postalCode: '94103',
+        state: 'CA',
+        stateCode: null,
+        street: '1040 Folsom St'
+      },
+      Administrative_Area__c: '6',
+      State_Upper_District__c: '11',
+      State_Lower_District__c: '17',
+      npo02__TotalOppAmount__c: 12345
+    },
+    {
+      attributes: {
+        type: 'Contact',
+        url: '/services/data/v40.0/sobjects/Contact/0034100000NrSgvAAF'
+      },
+      Name: 'Clemens Posch',
+      Phone: '8012345678',
+      Email: 'dc3@example.com',
+      MailingAddress: {
+        city: 'San Francisco',
+        country: 'US',
+        countryCode: null,
+        geocodeAccuracy: null,
+        latitude: 37.7779536,
+        longitude: -122.4067138,
+        postalCode: '94103',
+        state: 'CA',
+        stateCode: null,
+        street: '1040 Folsom St'
+      },
+      Administrative_Area__c: '6',
+      State_Upper_District__c: '11',
+      State_Lower_District__c: '17',
+      npo02__TotalOppAmount__c: 12345
+    }
+  ]
+}
+
 export default class Contacts extends React.Component {
   constructor (props) {
     super()
@@ -18,7 +125,6 @@ export default class Contacts extends React.Component {
       token: null,
       contacts: [],
       filteredContacts: [],
-      filterExport: '',
       select: null,
       hover: null,
       viewport: null,
@@ -35,18 +141,28 @@ export default class Contacts extends React.Component {
     const token = this._setTokenFromLocalStorage()
     if (token) {
       this._fetchContacts(token)
+    } else if (window.location.href.includes(':8080')) {
+      // local testing
+      this._handleSalesforceContacts(API_SAMPLE_RESPONSE)
     }
   }
 
   render () {
-    const {token, contacts, filteredContacts, filterExport, select, viewport, filter} = this.state
+    const {
+      token,
+      contacts,
+      filteredContacts,
+      select,
+      viewport,
+      filter
+    } = this.state
 
     return (
       <div>
         <Map
           layers={[this._renderScatterplotLayer()]}
           viewport={viewport}
-          onChangeViewport={(v) => this.setState({viewport: v})}
+          onChangeViewport={v => this.setState({ viewport: v })}
         />
         {select ? <ContactDetails person={select} /> : null}
         <Controls
@@ -54,20 +170,19 @@ export default class Contacts extends React.Component {
           onLogOut={() => this._logOut()}
           contacts={contacts}
           filteredContacts={filteredContacts}
-          filterExport={filterExport}
           district={filter.district}
           radius={filter.radius}
           radiusAddress={filter.radiusAddress}
-          onChangeDistrict={(district) => this._handleChangeDistrict(district)}
-          onChangeRadius={(radius) => this._handleChangeRadius(radius)}
-          onChangeRadiusAddress={(addr) => this._handleChangeRadiusAddress(addr)}
+          onChangeDistrict={district => this._handleChangeDistrict(district)}
+          onChangeRadius={radius => this._handleChangeRadius(radius)}
+          onChangeRadiusAddress={addr => this._handleChangeRadiusAddress(addr)}
         />
       </div>
     )
   }
 
   _renderScatterplotLayer () {
-    const {contacts, hover, select, viewport} = this.state
+    const { contacts, hover, select, viewport } = this.state
     const zoom = viewport ? viewport.zoom : 12
 
     return new ScatterplotLayer({
@@ -75,73 +190,125 @@ export default class Contacts extends React.Component {
       opacity: 1,
       radiusScale: 1,
       pickable: true,
-      getPosition: (contact) => {
-        return [contact.address.longitude, contact.address.latitude]
+      getPosition: contact => {
+        return [contact.location.longitude, contact.location.latitude]
       },
-      getRadius: (row) => {
+      getRadius: row => {
         const dotM = 2200 * Math.pow(0.7, zoom)
-        return dotM * ((row === select) ? 2.3 : 2)
+        return dotM * (row === select ? 2.3 : 2)
       },
-      getColor: (row) => {
-        const opacity = (row === select || row === hover) ? 1 : 0.8
+      getColor: row => {
+        const opacity = row === select || row === hover ? 1 : 0.8
         const rgb = row.isFiltered ? [100, 100, 100] : [0, 150, 0]
         return [rgb[0], rgb[1], rgb[2], Math.floor(255 * opacity)]
       },
-      onHover: (info) => {
+      onHover: info => {
         if (info.object === this.state.select) return
-        this.setState({hover: info.object})
+        this.setState({ hover: info.object })
       },
-      onClick: (info) => {
-        this.setState({select: info.object, hover: null})
+      onClick: info => {
+        this.setState({ select: info.object, hover: null })
       },
       updateTriggers: {
-        all: Object.assign({}, this.state.filter, {hover, select, zoom})
+        all: Object.assign({}, this.state.filter, { hover, select, zoom })
       }
     })
   }
 
   _setTokenFromLocalStorage () {
     const token = window.localStorage.oauth_access_token
-    this.setState({token})
+    this.setState({ token })
     return token
   }
 
   _fetchContacts (token) {
-    const url = API_ROOT + '/query?q=SELECT Name, Phone, Email, MailingAddress, ' +
+    const url =
+      API_ROOT +
+      '/query?q=SELECT Name, Phone, Email, MailingAddress, ' +
       'Administrative_Area__c, State_Upper_District__c, State_Lower_District__c, ' +
       'npo02__TotalOppAmount__c FROM Contact WHERE MailingLatitude != NULL'
-    const headers = {'Authorization': 'OAuth ' + token}
+    const headers = { Authorization: 'OAuth ' + token }
     fetch(url, headers, (err, data) => {
       if (err) {
         console.log('Fetch failed', err)
         return this._logOut()
       }
 
-      const contacts = data.records.map((r, i) => ({
-        index: i,
-        name: r.Name,
-        email: r.Email,
-        phone: r.Phone,
-        address: r.MailingAddress,
-        districts: {
-          city: r.Administrative_Area__c,
-          stateLower: r.State_Upper_District__c,
-          stateUpper: r.State_Upper_District__c
-        },
-        totalDonationsUSD: r.npo02__TotalOppAmount__c
-      }))
-
-      console.log('salesforce raw query response', data)
-      console.log('salesforce contacts', contacts)
-
-      this._filterContacts(this.state.filter, contacts)
+      this._handleSalesforceContacts(data)
     })
 
     // For debugging
-    window.fetchSalesforce = (path) => fetch(API_ROOT + path, headers, (err, data) => {
-      if (err) return console.error(err)
-      console.log(data)
-    })
+    window.fetchSalesforce = path =>
+      fetch(API_ROOT + path, headers, (err, data) => {
+        if (err) return console.error(err)
+        console.log(data)
+      })
+  }
+
+  _handleSalesforceContacts (data) {
+    const eps = 0.0002
+    const locs = {}
+    const cartSpiral = [
+      [0, 0],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1, 1],
+      [-1, 0],
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [2, -1],
+      [2, 0],
+      [2, 1],
+      [2, 2],
+      [1, 2],
+      [0, 2],
+      [-1, 2],
+      [-2, 2],
+      [-2, 1],
+      [-2, 0]
+    ]
+    const nextLoc = function (addr) {
+      if (addr == null || addr.longitude == null) {
+        return null
+      }
+      const ixLon = Math.round(addr.longitude / eps)
+      const ixLat = Math.round(addr.latitude / eps)
+      for (var i = 0; i < cartSpiral.length; i++) {
+        const offsets = cartSpiral[i]
+        const locKey = ixLon + offsets[0] + ',' + (ixLat + offsets[1])
+        if (locs[locKey] == null) {
+          locs[locKey] = true
+          return {
+            longitude: (ixLon + offsets[0]) * eps,
+            latitude: (ixLat + offsets[1]) * eps
+          }
+        }
+      }
+      console.warn('WARNING: failed to find free grid location', addr)
+      return addr.location
+    }
+
+    const contacts = data.records.map((r, i) => ({
+      index: i,
+      name: r.Name,
+      email: r.Email,
+      phone: r.Phone,
+      address: r.MailingAddress,
+      location: nextLoc(r.MailingAddress),
+      districts: {
+        city: r.Administrative_Area__c,
+        stateLower: r.State_Upper_District__c,
+        stateUpper: r.State_Upper_District__c
+      },
+      totalDonationsUSD: r.npo02__TotalOppAmount__c
+    }))
+
+    console.log('salesforce raw query response', data)
+    console.log('salesforce contacts', contacts)
+
+    this._filterContacts(this.state.filter, contacts)
   }
 
   _logOut () {
@@ -154,21 +321,24 @@ export default class Contacts extends React.Component {
     if (this.state.filter.district === district) {
       district = null
     }
-    this._setFilter({district})
+    this._setFilter({ district })
   }
 
   _handleChangeRadius (radius) {
-    this._setFilter({radius})
+    this._setFilter({ radius })
   }
 
   _handleChangeRadiusAddress (address) {
     if (address === '') {
-      return this._setFilter({radiusAddress: null})
+      return this._setFilter({ radiusAddress: null })
     }
 
     // Geocode via Mapbox
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-      window.encodeURIComponent(address) + '.json?access_token=' + config.MAPBOX_TOKEN
+    const url =
+      'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+      window.encodeURIComponent(address) +
+      '.json?access_token=' +
+      config.MAPBOX_TOKEN
     fetch(url, (err, data) => {
       if (err) console.error(err)
       console.log(data)
@@ -188,14 +358,14 @@ export default class Contacts extends React.Component {
           success: false
         }
       }
-      this._setFilter({radiusAddress})
+      this._setFilter({ radiusAddress })
     })
   }
 
   _readOauthAccessToken () {
     const oauthParams = ['access_token']
     const hash = document.location.hash.substring(1)
-    hash.split('&').forEach((part) => {
+    hash.split('&').forEach(part => {
       const kv = part.split('=')
       const k = kv[0]
       const v = window.decodeURIComponent(kv[1])
@@ -213,25 +383,22 @@ export default class Contacts extends React.Component {
   }
 
   _filterContacts (filter, contacts) {
-    var filteredContacts = contacts.filter((contact) => {
+    var filteredContacts = contacts.filter(contact => {
       let ret = true
       if (filter.radiusAddress && filter.radiusAddress.success) {
-        var dist = geo.computeDistance(contact.address, filter.radiusAddress.coordinates)
+        var dist = geo.computeDistance(
+          contact.address,
+          filter.radiusAddress.coordinates
+        )
         if (dist > filter.radius) ret = false
       }
       if (filter.district !== null) {
-        if (contact.districts.city !== ('' + filter.district)) ret = false
+        if (contact.districts.city !== '' + filter.district) ret = false
       }
       contact.isFiltered = !ret
       return ret
     })
 
-    const filterExport = filteredContacts.length === contacts.length
-      ? ''
-      : filteredContacts
-          .filter((c) => !!c.email)
-          .map((c) => c.name + '<' + c.email + '>').join(', ')
-
-    this.setState({filter, contacts, filteredContacts, filterExport})
+    this.setState({ filter, contacts, filteredContacts })
   }
 }
